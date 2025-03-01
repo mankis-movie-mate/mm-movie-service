1288432e90e6e95460c0eb60b397f2a85ba0e05b
tsayvyac <tsayvyac@fel.cvut.cz>
2025-03-01 16:51:03 +0100

    chore: add prefixes for env variables

--- app/config/config.js
+++ app/config/config.js
@@ -2,8 +2,8 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 module.exports = {
-  port: process.env.PORT || '3000',
-  mongo_uri: process.env.MONGO_URI,
-  logger_level: process.env.LOGGER_LEVEL || 'info',
-  base_url: process.env.BASE_URL,
+  port: process.env.MOVIE_MATE_MOVIE_SERVICE_PORT || '3000',
+  mongo_uri: process.env.MOVIE_MATE_MOVIE_SERVICE_MONGO_URI,
+  logger_level: process.env.MOVIE_MATE_MOVIE_SERVICE_LOGGER_LEVEL || 'info',
+  base_url: process.env.MOVIE_MATE_MOVIE_SERVICE_BASE_URL,
 };
