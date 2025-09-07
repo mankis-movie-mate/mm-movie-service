71d82b8d4f2f9fc1c1e25e2f5727e9d178992660
ulcheyev <ulcheyev@fel.cvut.cz>
2025-09-07 13:39:57 +0200

    [Upd] testing with cors

--- app/index.js
+++ app/index.js
@@ -9,21 +9,7 @@ const errorHandler = require('./middleware/errorHandler');
 const logger = require('./middleware/logger');
 const { allowedOrigins } = require('./config/cors');
 
-app.use(express.json());
-app.use(`${base_url}/docs`, swaggerUI.serve, swaggerUI.setup(swaggerSpec));
-app.use(`${base_url}`, movieRoutes);
-app.use(`${base_url}/manage`, movieManagementRoutes);
 
-
-app.get('/health', (req, res) => {
-    logger.info('Health check OK');
-    res.send('OK');
-});
-
-app.get(`/docs/swagger.json`, (req, res) => res.send(swaggerSpec));
-
-// middleware
-app.use(errorHandler);
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin) return callback(null, true);
@@ -34,5 +20,18 @@ app.use(cors({
     },
     credentials: true,
 }));
+app.use(express.json());
+app.use(`${base_url}/docs`, swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.use(`${base_url}`, movieRoutes);
+app.use(`${base_url}/manage`, movieManagementRoutes);
+app.get('/health', (req, res) => {
+    logger.info('Health check OK');
+    res.send('OK');
+});
+app.get(`/docs/swagger.json`, (req, res) => res.send(swaggerSpec));
+
+// middleware
+app.use(errorHandler);
+
 
 module.exports = app;
